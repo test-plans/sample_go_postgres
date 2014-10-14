4cb9b19d9ef0b8351f1f33bf5b554bad4079d0ad
buildsample <buildsampletest@gmail.com>
2014-10-14 18:27:03 +0530

    Update sample_go_postgres_test.go

--- sample_go_postgres_test.go
+++ sample_go_postgres_test.go
@@ -9,7 +9,7 @@ var _ = Describe("Sample", func() {
 	Describe("Saving a name in db", func() {
 		Context("and then returning it back", func() {
 			It("should result to 'Daniel'", func() {
-				Expect(GetResult()).To(Equal("Daniel"))
+				Expect(GetResult()).To(Equal("Danie"))
 			})
 		})
 	})
